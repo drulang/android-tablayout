0e44ea8dcdfacf768b49986f231148d28da5ec44
Dru Lang <dlang@tripadvisor.com>
2018-02-14 10:43:21 -0500

    add proj

---
+++ Tabby/app/src/main/java/com/drulang/tabby/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.drulang.tabby;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by dru2 on 2/14/18.
+ */
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+    private Context mContext;
+
+    public SimpleFragmentPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return new PlacesFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Places";
+            case 1:
+                return "Settings";
+        }
+
+        return null;
+    }
+}
